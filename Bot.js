9d8b1680f9c724944396ee32ea453138a4095fc4
mnagi5370-boop <mnagi5370@gmail.com>
2025-11-15 18:03:12 +0200

    Bot.js
    Added an Express server to keep the bot alive.

--- Bot.js
+++ Bot.js
@@ -28,3 +28,17 @@ client.on("messageCreate", (message) => {
 
 // تسجيل الدخول باستخدام التوكن
 client.login(config.token);
+const express = require("express");
+const server = express();
+
+server.all("/", (req, res) => {
+  res.send("Bot is Running!");
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Server is Ready!");
+  });
+}
+
+keepAlive();
